6ebbd47e663095043e518d4115f6a8a96a862c05
mx <maks064040@gmail.com>
2025-11-25 20:29:36 +0200

    trigger

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ const functions = require('@google-cloud/functions-framework');
 functions.http('helloHttp', (req, res) => {
     res.set('Content-Type', 'text/plain');
 
-
     res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
 });
